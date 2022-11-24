0b421abe2ea45d992ab9c894b40d3c15a37719c5
rzeydelis <bobbyzeydelis@gmail.com>
2022-11-24 18:31:55 -0500

    Add files via upload

---
+++ balance_checker.py
@@ -0,0 +1,42 @@
+import os
+import re
+
+
+def token_bal_check():
+    os.system("node uniswap_test_pull.js")
+    file = open('logresults.txt', mode = 'r', encoding = 'utf-8-sig')
+    lines = file.readlines()
+    file.close()
+
+    tokenA = 0
+    tokenB = 0
+    count = 0
+    for line in lines:
+        count+=1
+        if count <= 2:
+            continue
+
+        if count == 3:
+            line = line.split(',')
+            line = [i.strip() for i in line]
+            # tokenA = (["'])(?:(?=(\\?))\2.)*?\1
+            tokenA = float(str(line).rsplit(':', 1)[1][:-2].strip())
+            # print("tokenA:", tokenA)
+
+        if count == 4:
+            line = line.split(',')
+            line = [i.strip() for i in line]
+            # tokenA = (["'])(?:(?=(\\?))\2.)*?\1
+            tokenB = float(str(line).rsplit(':', 1)[1][:-2].strip())
+            # print("tokenB:", tokenB)        
+
+        # print(line)
+        # count+=1
+
+    min_req_token_bal = (tokenA + tokenB) * .1
+    if tokenA < min_req_token_bal: # tokenBal shouldn't be under 10% of the entire LP amount
+        print("Token 1 has a low balance.")
+    elif tokenB < min_req_token_bal:
+        print("Token 2 has a low balance.")
+    else:
+        print("Token 1 & 2 balances are not in danger zone.")
---
+++ main.py
@@ -0,0 +1,4 @@
+from balance_checker import token_bal_check
+import os
+
+token_bal_check()
